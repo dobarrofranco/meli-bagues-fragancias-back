9a4454c6653d70ae5ea226d9c4828042440b49cc
dobarrofranco <mr.francodobarro@gmail.com>
2023-11-14 02:49:50 -0300

    deploy settings

--- src/db.js
+++ src/db.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const ProductModel = require('./models/Product')
 const FraganceModel = require('./models/Fragance')
 
-const sequelize = new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/products`,
+// const sequelize = new Sequelize(
+//     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/products`,
+//     { logging: false, native: false }
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY,
     { logging: false, native: false }
 );
 
