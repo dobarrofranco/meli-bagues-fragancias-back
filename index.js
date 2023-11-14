7e0a4c4f80f925b595f08922fbbeb72e86c7e29f
dobarrofranco <mr.francodobarro@gmail.com>
2023-11-14 02:55:14 -0300

    deploy settings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const insertProducts = require('./seeders/insertProducts');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
